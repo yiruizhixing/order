ba4aa78da45fffc992844034dd524ce7e9c4c2a0
yiruizhixing <52438686+yiruizhixing@users.noreply.github.com>
2020-05-02 19:35:44 +0800

    20200502-1

--- config/production_setting.py
+++ config/production_setting.py
@@ -1,6 +1,12 @@
 # 生产环境
-DEBUG = False
+DEBUG = True
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = 'mysql://food_db:yirui123+@127.0.0.1/food_db'
+SQLALCHEMY_DATABASE_URI = 'mysql://food_db:yirui123+@127.0.0.1/food_db?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_ENCODING = "utf-8"
+SQLALCHEMY_ENCODING = "utf8mb4"
+
+APP = {
+    'domain':'https://gongkao.org.cn'
+}
+
+RELEASE_VERSION="20200502"
